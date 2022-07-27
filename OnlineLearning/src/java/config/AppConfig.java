52b3cbcb2df99367c2b04bae969ceaa9269fe4dc
chunglh <lhchung306@gmail.com>
2022-07-27 17:08:00 +0700

    project online learning swp391

---
+++ OnlineLearning/src/java/config/AppConfig.java
@@ -0,0 +1,10 @@
+package config;
+
+public class AppConfig {
+
+    public final static String USERNAME_EMAIL = "chunglhhe153630@fpt.edu.vn";
+    public final static String PASSWORD_EMAIL = "chung3062k1";
+    public final static String LINK_VERIFY = "http://localhost:8080/OnlineLearning/verify";
+    public final static String LINK_CHANGE_PASSWORD = "http://localhost:8080/OnlineLearning/check-expired";
+    public final static String LINK_CHANGE_PASSWORD_WHEN_ADD = "http://localhost:8080/OnlineLearning/modified";
+}
